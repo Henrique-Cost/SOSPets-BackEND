ef471ae1c429e2bd983723e46cec5de81f819e14
Daniel Mariano Corazza <81099633+Daniel-Corazza-reg1sem2021@users.noreply.github.com>
2022-03-15 20:36:25 -0300

    Add files via upload

---
+++ src/main/java/com/tcc/sospets/SospetsApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.sospets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SospetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SospetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/sospets/SospetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.sospets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SospetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
