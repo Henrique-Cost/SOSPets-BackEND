a53fd42dc7beb881fddcdc5c3f2da182db082e3d
unknown <32174@fiecalunos.lan>
2022-03-17 20:46:57 -0300

    init

--- src/main/java/com/tcc/sospets/business/models/Usuario.java
+++ src/main/java/com/tcc/sospets/business/models/Usuario.java
@@ -1,4 +1,29 @@
 package com.tcc.sospets.business.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "Usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @ToString.Exclude
+    private String id_usuario;
+
+    private String nomeCompleto;
+    private String email;
+    private String telefone;
+    private String senha;
+    private String tipo;
+
 }
