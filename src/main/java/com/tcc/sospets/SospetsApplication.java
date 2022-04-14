9b4eda12faa76073d05d5f6240a3d69e0b81be39
Daniel Corazza <daniel.corazza@vilt.local>
2022-04-14 18:17:05 -0300

    Fiz quase nada, mandando só pra n esquecer depois

--- src/main/java/com/tcc/sospets/SospetsApplication.java
+++ src/main/java/com/tcc/sospets/SospetsApplication.java
@@ -3,6 +3,10 @@ package com.tcc.sospets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 @SpringBootApplication
 public class SospetsApplication {
@@ -10,11 +14,11 @@ public class SospetsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SospetsApplication.class, args);
 
-		//try {
-		//	Files.createDirectory(Paths.get("uploads"));
-		//} catch (IOException e) {
-		//	e.printStackTrace();
-		//}
+		try {
+			Files.createDirectory(Paths.get("uploads"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
